0625157dfe59fac8d5a47e8ae28697c65037f367
freelook27 <freelook27@gmail.com>
2022-05-18 10:38:00 +0300

    update

---
+++ test_first_api.py
@@ -0,0 +1,20 @@
+import requests
+
+class TestFirstApi:
+    def test_hello_call(self):
+        url = 'http://playground.learnqa.ru/api/hello'
+        name = 'Vitalii'
+        data = {'name': name}
+
+        response = requests.get(url, params=data)
+        
+        assert response.status_code == 200, "Wrong response code"
+
+        response_dict = response.json()
+        assert "answer" in response_dict, "There is no field 'answer' in response"
+
+
+        expected_respose_text = f"Hello, {name}"
+        actual_response_text = response_dict["answer"]
+        assert actual_response_text == expected_respose_text, "Actual text in the response is not correct"
+        
